8a91ebd38150fe80e8c75483c1af91f8802399d2
ocheresh <sashachereshnevy@gmail.com>
2019-10-12 16:22:33 +0300

    add check in main

--- src/main/java/ocheresh/swingy/Runner.java
+++ src/main/java/ocheresh/swingy/Runner.java
@@ -2,43 +2,28 @@ package ocheresh.swingy;
 
 import ocheresh.swingy.data.Data;
 import ocheresh.swingy.data.ScanInfo;
-import ocheresh.swingy.model.characthers.CharacthersFactory;
-import ocheresh.swingy.model.characthers.EnumSuperHero;
-import ocheresh.swingy.model.characthers.SuperHero;
-import ocheresh.swingy.view.game.GameFrame;
-import ocheresh.swingy.view.select.SelectFrame;
 import ocheresh.swingy.view.start.MainConsole;
 import ocheresh.swingy.view.start.MainFrame;
-
-import javax.swing.*;
-import java.util.Scanner;
 
 
 public class Runner {
 
-
-
-    public static JFrame jFrame;
-//    public static Statement statmt;
-//    public static ResultSet resSet;
-
     public static void main(String[] args)
     {
+
+        if ((args.length != 1) || (!(args[0].equals("gui")) && !(args[0].equals("console"))))
+        {
+            System.out.println("You must enter gui or console");
+            System.exit(0);
+        }
+
         Data.connect();
         ScanInfo.connect_scan();
-
-        if (args[0].contentEquals("gui")) {
+        if (args[0].equals("gui")) {
             new MainFrame();
         }
-        else if (args[0].contentEquals("console")) {
+        else if (args[0].equals("console")) {
             new MainConsole();
         }
-        else {
-            System.out.println("You must enter gui or console");
-            return;
-        }
-
-//        Data.closeDB();
-
     }
 }
